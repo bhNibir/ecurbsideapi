46e557a4d4faadd0e7ba4567e7cb83c1ab016038
Biplob Hasan Nibir <33071550+bhNibir@users.noreply.github.com>
2023-08-09 10:56:31 +0000

    fix url

--- core/settings.py
+++ core/settings.py
@@ -104,6 +104,8 @@ CORS_ALLOWED_ORIGINS = [
 "http://127.0.0.1:8000",
 'http://localhost:8000',
 'https://ecurbside-17b9c.web.app',
+'https://ecurbside.web.app',
+'https://ecrubside.web.app',
 ]
 
 
